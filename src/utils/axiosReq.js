24bc422e4cf2eebf40f9ec5cbda478f6d4990550
poshcoderbd <poshcoderbd@gmail.com>
2026-08-07 01:43:44 +0600

    Update axiosReq.js

--- src/utils/axiosReq.js
+++ src/utils/axiosReq.js
@@ -8,3 +8,14 @@ export const axiosReq = axios.create({
   baseURL: baseURL,
   withCredentials: true
 });
+
+
+axiosReq.interceptors.request.use((config) => {
+  const token = localStorage.getItem("poshcoder_admin");
+
+  if (token) {
+    config.headers.authorization = token;
+  }
+
+  return config;
+});
