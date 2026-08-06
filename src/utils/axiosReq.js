6df6fc8be05da405004a476ef82c54c275c2acbe
poshcoderbd <poshcoderbd@gmail.com>
2026-08-07 01:39:36 +0600

    Reverse axiosReq.js

--- src/utils/axiosReq.js
+++ src/utils/axiosReq.js
@@ -8,14 +8,3 @@ export const axiosReq = axios.create({
   baseURL: baseURL,
   withCredentials: true
 });
-
-
-axiosReq.interceptors.request.use((config) => {
-  const token = localStorage.getItem("poshcoder_admin");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
-});
